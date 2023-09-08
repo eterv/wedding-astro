53f204b3da15c46fe769a7103c70a6b25334259c
Lucas Choi <eterv7@gmail.com>
2023-09-08 19:56:37 +0900

    Install eslint, prettier, tailwind

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+};
