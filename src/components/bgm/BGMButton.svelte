f39f1e7ac8bccf0aa9d40e28d8caea6902defbeb
Lucas Choi <eterv7@gmail.com>
2023-09-15 17:20:30 +0900

    feat: Added BGM components

---
+++ src/components/bgm/BGMButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let isPlaying: boolean;
+</script>
+
+<button
+  class={[
+    'flex items-center justify-center',
+    'h-40 w-40',
+    'rounded-full border-3 border-black3',
+  ].join(' ')}
+  class:playing={isPlaying}
+  on:click
+>
+  <span class="block h-20 w-20 bg-contain bg-center bg-no-repeat" />
+</button>
+
+<style>
+  button span {
+    background-image: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="M5 5.274c0-1.707 1.826-2.792 3.325-1.977l12.362 6.726c1.566.853 1.566 3.101 0 3.953L8.325 20.702C6.826 21.518 5 20.432 5 18.726V5.274Z"%2F%3E%3C%2Fsvg%3E');
+  }
+
+  button.playing span {
+    background-image: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="currentColor" d="M5.746 3a1.75 1.75 0 0 0-1.75 1.75v14.5c0 .966.784 1.75 1.75 1.75h3.5a1.75 1.75 0 0 0 1.75-1.75V4.75A1.75 1.75 0 0 0 9.246 3h-3.5Zm9 0a1.75 1.75 0 0 0-1.75 1.75v14.5c0 .966.784 1.75 1.75 1.75h3.5a1.75 1.75 0 0 0 1.75-1.75V4.75A1.75 1.75 0 0 0 18.246 3h-3.5Z"%2F%3E%3C%2Fsvg%3E');
+  }
+</style>
