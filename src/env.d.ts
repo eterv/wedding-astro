9182fc9cebf6f4cd2c8b5635d9526284d5adb0be
Lucas Choi <eterv7@gmail.com>
2023-09-15 17:20:50 +0900

    fix: Deleted cloudflare types in astro env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="astro/client" />
-
-type AdvancedRuntime = import('@astrojs/cloudflare').AdvancedRuntime;
-
-declare namespace App {
-  interface Locals extends AdvancedRuntime {
-    // runtime: {
-    //   env: {
-    //     KV: string;
-    //   };
-    // };
-  }
-}
